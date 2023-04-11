53dffdb90b0dc436c4e51fc852043623c5d6173d
Rafael KozAnd <rafandreola@hotmail.com>
2023-04-11 19:14:28 -0300

    .

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,8 +1,14 @@
-import { makeRedirectUri, revokeAsync, startAsync } from 'expo-auth-session';
-import React, { useEffect, createContext, useContext, useState, ReactNode } from 'react';
-import { generateRandom } from 'expo-auth-session/build/PKCE';
+import { makeRedirectUri, revokeAsync, startAsync } from "expo-auth-session";
+import React, {
+  useEffect,
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+} from "react";
+import { generateRandom } from "expo-auth-session/build/PKCE";
 
-import { api } from '../services/api';
+import { api } from "../services/api";
 
 interface User {
   id: number;
@@ -19,6 +25,15 @@ interface AuthContextData {
   signOut: () => Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    error: string;
+    state: string;
+    access_token: string;
+  };
+  type: string;
+}
+
 interface AuthProviderData {
   children: ReactNode;
 }
@@ -26,78 +41,127 @@ interface AuthProviderData {
 const AuthContext = createContext({} as AuthContextData);
 
 const twitchEndpoints = {
-  authorization: 'https://id.twitch.tv/oauth2/authorize',
-  revocation: 'https://id.twitch.tv/oauth2/revoke'
+  authorization: "https://id.twitch.tv/oauth2/authorize",
+  revocation: "https://id.twitch.tv/oauth2/revoke",
 };
 
 function AuthProvider({ children }: AuthProviderData) {
   const [isLoggingOut, setIsLoggingOut] = useState(false);
   const [isLoggingIn, setIsLoggingIn] = useState(false);
   const [user, setUser] = useState({} as User);
-  const [userToken, setUserToken] = useState('');
+  const [userToken, setUserToken] = useState("");
 
   // get CLIENT_ID from environment variables
+  const { CLIENT_ID } = process.env;
 
   async function signIn() {
     try {
       // set isLoggingIn to true
-
       // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
       // RESPONSE_TYPE - set to "token"
       // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
       // FORCE_VERIFY - set to true
       // STATE - generate random 30-length string using generateRandom() with "size" set to 30
-
-      // assemble authUrl with twitchEndpoint authorization, client_id, 
+      // assemble authUrl with twitchEndpoint authorization, client_id,
       // redirect_uri, response_type, scope, force_verify and state
-
       // call startAsync with authUrl
-
       // verify if startAsync response.type equals "success" and response.params.error differs from "access_denied"
       // if true, do the following:
+      // verify if startAsync response.params.state differs from STATE
+      // if true, do the following:
+      // throw an error with message "Invalid state value"
+      // add access_token to request's authorization header
+      // call Twitch API's users route
+      // set user state with response from Twitch API's route "/users"
+      // set userToken state with response's access_token from startAsync
+      setIsLoggingIn(true);
 
-        // verify if startAsync response.params.state differs from STATE
-        // if true, do the following:
-          // throw an error with message "Invalid state value"
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true });
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("openid user:read:email user:read:follows");
+      const FORCE_VERIFY = true;
+      const STATE = generateRandom(30);
 
-        // add access_token to request's authorization header
+      const authUrl =
+        twitchEndpoints.authorization +
+        `?client_id=${CLIENT_ID}` +
+        `&redirect_uri=${REDIRECT_URI}` +
+        `&response_type=${RESPONSE_TYPE}` +
+        `&scope=${SCOPE}` +
+        `&force_verify=${FORCE_VERIFY}` +
+        `&state=${STATE}`;
 
-        // call Twitch API's users route
+      const { type, params } = (await startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
 
-        // set user state with response from Twitch API's route "/users"
-        // set userToken state with response's access_token from startAsync
+      if (type === "success" && params.error !== "access_denied") {
+        if (params.state !== STATE) {
+          throw new Error("Invalid state value");
+        }
+
+        api.defaults.headers.authorization = `Bearer ${params.access_token}`;
+
+        const userResponse = await api.get("/users");
+
+        // setUser(userResponse.data[0]);
+        setUser({
+          id: userResponse.data.data[0].id,
+          display_name: userResponse.data.data[0].display_name,
+          email: userResponse.data.data[0].email,
+          profile_image_url: userResponse.data.data[0].profile_image_url,
+        });
+        setUserToken(params.access_token);
+        console.log(userResponse);
+      }
     } catch (error) {
-      // throw an error
+      // throw an error'
+      throw new Error();
     } finally {
       // set isLoggingIn to false
+      setIsLoggingIn(false);
     }
   }
 
   async function signOut() {
     try {
       // set isLoggingOut to true
-
       // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      setIsLoggingOut(true);
+      await revokeAsync(
+        {
+          token: userToken,
+          clientId: CLIENT_ID,
+        },
+        {
+          revocationEndpoint: twitchEndpoints.revocation,
+        }
+      );
     } catch (error) {
     } finally {
       // set user state to an empty User object
       // set userToken state to an empty string
-
       // remove "access_token" from request's authorization header
-
       // set isLoggingOut to false
+      setUser({} as User);
+      setUserToken("");
+      delete api.defaults.headers.authorization;
+      setIsLoggingOut(false);
     }
   }
 
   useEffect(() => {
     // add client_id to request's "Client-Id" header
-  }, [])
+    api.defaults.headers["Client-Id"] = CLIENT_ID;
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}>
-      { children }
+    <AuthContext.Provider
+      value={{ user, isLoggingOut, isLoggingIn, signIn, signOut }}
+    >
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 function useAuth() {
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,40 +1,40 @@
-import { useTheme } from 'styled-components';
-import React, { useEffect, useState } from 'react';
-import { Alert, FlatList, Modal, ActivityIndicator, View } from 'react-native';
-import { Feather } from '@expo/vector-icons'
+import { useTheme } from "styled-components";
+import React, { useEffect, useState } from "react";
+import { Alert, FlatList, Modal, ActivityIndicator, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-import { TopGamesCard } from '../../components/TopGamesCard';
-import { useAuth } from '../../hooks/useAuth';
-import { api } from '../../services/api';
+import { TopGamesCard } from "../../components/TopGamesCard";
+import { useAuth } from "../../hooks/useAuth";
+import { api } from "../../services/api";
 
 import {
-  Container, 
-  Header, 
-  UserInfo, 
-  Avatar, 
+  Container,
+  Header,
+  UserInfo,
+  Avatar,
   UserInfoText,
-  SignOutButton, 
-  UserFollowedStreams, 
-  UserFollowedStreamsTitle, 
-  TopGames, 
-  TopGamesTitle
-} from './styles';
-import { UserFollowedStreamCard } from '../../components/UserFollowedStreamCard';
+  SignOutButton,
+  UserFollowedStreams,
+  UserFollowedStreamsTitle,
+  TopGames,
+  TopGamesTitle,
+} from "./styles";
+import { UserFollowedStreamCard } from "../../components/UserFollowedStreamCard";
 
 interface TopGames {
-  box_art_url: string, 
-  id: string, 
-  name: string
+  box_art_url: string;
+  id: string;
+  name: string;
 }
 
 interface UserFollowedStreams {
   id: string;
-  thumbnail_url: string, 
-  title: string,
-  user_id: string, 
-  user_login: string, 
-  user_name: string,
-  viewer_count: number
+  thumbnail_url: string;
+  title: string;
+  user_id: string;
+  user_login: string;
+  user_name: string;
+  viewer_count: number;
 }
 
 interface UserFollowedStreamsFormatted extends UserFollowedStreams {
@@ -43,64 +43,90 @@ interface UserFollowedStreamsFormatted extends UserFollowedStreams {
 
 export function Home() {
   const [topGames, setTopGames] = useState<TopGames[]>([]);
-  const [userFollowedStreams, setUserFollowedStreams] = useState<UserFollowedStreamsFormatted[]>([]);
-  const [isLoadingUserFollowedStreams, setIsLoadingUserFollowedStreams] = useState(true);
+  const [userFollowedStreams, setUserFollowedStreams] = useState<
+    UserFollowedStreamsFormatted[]
+  >([]);
+  const [isLoadingUserFollowedStreams, setIsLoadingUserFollowedStreams] =
+    useState(true);
   const [isLoadingTopGames, setIsLoadingTopGames] = useState(true);
-  
+
   const theme = useTheme();
   const { signOut, user, isLoggingOut } = useAuth();
 
   // creates a function to handle sign out
-    // try to call and wait signOut
-    // if fails, display an Alert with the title "Erro SignOut" and message "Ocorreu um erro ao tentar se deslogar do app"
+  // try to call and wait signOut
+  // if fails, display an Alert with the title "Erro SignOut" and message "Ocorreu um erro ao tentar se deslogar do app"
+  function handleSignOut() {
+    try {
+      signOut();
+    } catch (error) {
+      Alert.alert("Erro SignOut", "Ocorreu um erro ao tentar deslogar no app");
+    }
+  }
 
   async function getTopGames() {
     try {
-      const response = await api.get('/games/top');
+      const response = await api.get("/games/top");
 
       setTopGames(response.data.data);
       setIsLoadingTopGames(false);
     } catch (error) {
-      Alert.alert('Erro Top Games', 'Ocorreu um erro ao buscar os jogos mais assistidos agora na Twitch');
+      Alert.alert(
+        "Erro Top Games",
+        "Ocorreu um erro ao buscar os jogos mais assistidos agora na Twitch"
+      );
     }
   }
 
-  async function getUserFollowedStreamsAvatar(userFollowedStreamsData: UserFollowedStreams[]) {
-    return Promise.all(userFollowedStreamsData.map(async (item) => {
+  async function getUserFollowedStreamsAvatar(
+    userFollowedStreamsData: UserFollowedStreams[]
+  ) {
+    return Promise.all(
+      userFollowedStreamsData.map(async (item) => {
         try {
           const response = await api.get(`/users?id=${item.user_id}`);
 
-          return { ...item, user_avatar_url: response.data.data[0].profile_image_url }
+          return {
+            ...item,
+            user_avatar_url: response.data.data[0].profile_image_url,
+          };
         } catch (error) {
-          return { ...item, user_avatar_url: 'https://static-cdn.jtvnw.net/user-default-pictures-uv/cdd517fe-def4-11e9-948e-784f43822e80-profile_image-300x300.png' }
+          return {
+            ...item,
+            user_avatar_url:
+              "https://static-cdn.jtvnw.net/user-default-pictures-uv/cdd517fe-def4-11e9-948e-784f43822e80-profile_image-300x300.png",
+          };
         }
       })
-    )
+    );
   }
 
   async function getUserFollowedStreams() {
     try {
-      const response = await api.get<{ data: UserFollowedStreams[] }>(`/streams/followed?user_id=${user.id}`);
+      const response = await api.get<{ data: UserFollowedStreams[] }>(
+        `/streams/followed?user_id=${user.id}`
+      );
 
-      const formattedResponse = await getUserFollowedStreamsAvatar(response.data.data);
-      
+      const formattedResponse = await getUserFollowedStreamsAvatar(
+        response.data.data
+      );
+
       if (formattedResponse) {
         setUserFollowedStreams(formattedResponse);
         setIsLoadingUserFollowedStreams(false);
       }
     } catch (error) {
-      Alert.alert('Erro User Followed Streams', 'Ocorreu um erro ao buscar as informações das streams ao vivo que o usuário segue');
+      Alert.alert(
+        "Erro User Followed Streams",
+        "Ocorreu um erro ao buscar as informações das streams ao vivo que o usuário segue"
+      );
     }
   }
 
   useEffect(() => {
     getTopGames();
     getUserFollowedStreams();
-  }, [])
-
-  // const signOutButtonProps = {
-  //   onPress: your-signOut-function
-  // }
+  }, []);
 
   return (
     <Container
@@ -122,35 +148,53 @@ export function Home() {
           <Avatar source={{ uri: user.profile_image_url }} />
 
           <UserInfoText>Olá, </UserInfoText>
-          <UserInfoText style={{ fontFamily: theme.fonts.bold }}>{user.display_name}</UserInfoText>
+          <UserInfoText style={{ fontFamily: theme.fonts.bold }}>
+            {user.display_name}
+          </UserInfoText>
         </UserInfo>
 
-        {/* <SignOutButton onPress={}>
-          Verify if isLoggingOut is true
+        <SignOutButton onPress={handleSignOut}>
+          {isLoggingOut ? (
+            <ActivityIndicator color={theme.colors.white} size={25} />
+          ) : (
+            <Feather name="power" color={theme.colors.white} size={24} />
+          )}
+          {/* Verify if isLoggingOut is true
           If it is, show an ActivityIndicator
-          Otherwise, show Feather's power icon
-        </SignOutButton> */}
+          Otherwise, show Feather's power icon */}
+        </SignOutButton>
       </Header>
 
       <UserFollowedStreams>
-        <UserFollowedStreamsTitle>Canais que você segue</UserFollowedStreamsTitle>
+        <UserFollowedStreamsTitle>
+          Canais que você segue
+        </UserFollowedStreamsTitle>
 
-        <FlatList 
-          data={!isLoadingUserFollowedStreams ? userFollowedStreams : [{ id: '1' } as UserFollowedStreamsFormatted, { id: '2' } as UserFollowedStreamsFormatted]}
-          keyExtractor={item => item.id}
+        <FlatList
+          data={
+            !isLoadingUserFollowedStreams
+              ? userFollowedStreams
+              : [
+                  { id: "1" } as UserFollowedStreamsFormatted,
+                  { id: "2" } as UserFollowedStreamsFormatted,
+                ]
+          }
+          keyExtractor={(item) => item.id}
           horizontal
           showsHorizontalScrollIndicator={false}
           maxToRenderPerBatch={4}
           initialNumToRender={4}
-          getItemLayout={(_, index) => (
-            { length: 276, offset: 276 * index, index }
-          )}
+          getItemLayout={(_, index) => ({
+            length: 276,
+            offset: 276 * index,
+            index,
+          })}
           contentContainerStyle={{
             paddingLeft: 24,
-            paddingRight: 12
+            paddingRight: 12,
           }}
           renderItem={({ item }) => (
-            <UserFollowedStreamCard 
+            <UserFollowedStreamCard
               avatarUrl={item.user_avatar_url}
               streamer_login={item.user_login}
               streamer_name={item.user_name}
@@ -166,19 +210,29 @@ export function Home() {
       <TopGames>
         <TopGamesTitle>Mais assistidos do momento</TopGamesTitle>
 
-        <FlatList 
-          data={!isLoadingTopGames ? topGames : [{ id: '1' } as TopGames, { id: '2' } as TopGames, { id: '3' } as TopGames]}
-          keyExtractor={item => item.id}
+        <FlatList
+          data={
+            !isLoadingTopGames
+              ? topGames
+              : [
+                  { id: "1" } as TopGames,
+                  { id: "2" } as TopGames,
+                  { id: "3" } as TopGames,
+                ]
+          }
+          keyExtractor={(item) => item.id}
           horizontal
           showsHorizontalScrollIndicator={false}
           maxToRenderPerBatch={5}
           initialNumToRender={5}
-          getItemLayout={(_, index) => (
-            { length: 166, offset: 166 * index, index }
-          )}
+          getItemLayout={(_, index) => ({
+            length: 166,
+            offset: 166 * index,
+            index,
+          })}
           contentContainerStyle={{
             paddingLeft: 24,
-            paddingRight: 8
+            paddingRight: 8,
           }}
           renderItem={({ item }) => (
             <TopGamesCard
@@ -191,15 +245,13 @@ export function Home() {
         />
       </TopGames>
 
-      <Modal 
+      <Modal
         animationType="fade"
         visible={isLoggingOut}
         statusBarTranslucent
         transparent
       >
-        <View
-          style={{ flex: 1, backgroundColor: 'rgba(14, 14, 16, 0.5)' }}
-        />
+        <View style={{ flex: 1, backgroundColor: "rgba(14, 14, 16, 0.5)" }} />
       </Modal>
     </Container>
   );
